c1a87aeeec6b31c39fa915cbfe59953646d690e6
luke2327 <qwepoi8526@gmail.com>
2021-12-10 23:50:18 +0900

    commit

--- api.ts
+++ api.ts
@@ -1,11 +1,42 @@
 const axios = require("axios");
 
+type Base = {
+  id: number,
+  title: string,
+  createdAt: string,
+  author: string,
+  brand: string,
+  link: string
+}
+
+type C005Banner = {
+  src: string,
+} & Base;
+
+// interface C005Banner extends Base {
+//   src: string,
+// };
+
+type C005Contents = Base;
+
+type C005 = {
+  banner: C005Banner[],
+  contents: C005Contents[]
+}
+
+type C001 = {
+  id: number,
+  brand: string,
+  src: string,
+  link: string
+}
+
 async function getContents001() {
   const data = await axios.get(
     "https://6bdhahizcf.execute-api.ap-northeast-2.amazonaws.com/lg/contents/C001"
   );
 
-  displayC001(data.data);
+  displayC001(data.data as C001[]);
 }
 
 async function getContents005() {
@@ -13,22 +44,26 @@ async function getContents005() {
     "https://6bdhahizcf.execute-api.ap-northeast-2.amazonaws.com/lg/contents/C005"
   );
 
-  displayC005(data.data);
+  const res: C005 = data.data;
+
+  displayC005(res);
 }
 
-function displayC005(data) {
+function displayC005(data: C005) {
   console.log("BANNER::");
+
   data.banner.forEach((value) => {
     console.log(value);
   });
 
   console.log("CONTENTS::");
+
   data.contents.forEach((value) => {
     console.log(value);
   });
 }
 
-function displayC001(data) {
+function displayC001(data: C001[]) {
   console.log("DATA::");
   data.forEach((value) => {
     console.log(value);
@@ -37,7 +72,7 @@ function displayC001(data) {
 
 async function run() {
   await getContents005();
-  await getContents001();
+  // await getContents001();
 }
 
 run();
---
+++ arithmeticOperation.ts
@@ -0,0 +1,21 @@
+function addition(a: number, b: number) {
+  return a + b;
+}
+
+function pow(a: number, b: number) {
+  return Math.pow(a, b);
+}
+
+function subtraction(a: number, b: number) {
+  return a - b;
+}
+
+function multiplication(a: number, b: number) {
+  return a * b;
+}
+
+function division(a: number, b: number) {
+  return a / b;
+}
+
+console.log(addition(2, 2));
